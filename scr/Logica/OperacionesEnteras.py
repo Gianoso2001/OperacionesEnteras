5f3fcfeb2584278f46f4b1759b5926aa98b8d2ae
Gianfranco <oagianfranco@gmail.com>
2023-05-17 20:17:33 -0500

    Linea base del proyecto

---
+++ scr/Logica/OperacionesEnteras.py
@@ -0,0 +1,3 @@
+class OperacionesEnteras:
+    def MCD(self):
+        pass
---
+++ test/PruebaOperacionesEnteras.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class PruebaOperacionesEnteras(unittest.TestCase):
+    def test_MCD(self):
+        self.assertEqual(True, False)
+
