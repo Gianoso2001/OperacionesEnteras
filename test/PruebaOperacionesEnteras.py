fba13d7dfe805448ac616fbcc98550537ef846f2
Gianfranco <oagianfranco@gmail.com>
2023-05-17 21:44:17 -0500

    MCD de tres números (verde)

--- test/PruebaOperacionesEnteras.py
+++ test/PruebaOperacionesEnteras.py
@@ -32,6 +32,7 @@ import unittest
 from src.logica.OperacionesEnteros import OperacionesEnteros
     class PruebaOperacionesEnteros(unittest.TestCase):
     def test_MCD_dosNumerosPositivos_retornaMCD(self):
+
     # Arrange
         numero1 = 18
         numero2 = 24
