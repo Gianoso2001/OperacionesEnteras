1f7995eba52b63c2fc311a887c18ad19b8e92daf
Gianfranco <oagianfranco@gmail.com>
2023-05-17 20:44:02 -0500

    MCD de dos números (verde)

--- src/Logica/OperacionesEnteras.py
+++ src/Logica/OperacionesEnteras.py
@@ -1,3 +1,12 @@
 class OperacionesEnteras:
     def MCD(self, numero1, numero2):
         return None
+
+    def MCD(self, numero1, numero2):
+        temporal = 0
+
+    while numero2 != 0:
+        temporal = numero2
+        numero2 = numero1 % numero2
+        numero1 = temporal
+    return numero1
